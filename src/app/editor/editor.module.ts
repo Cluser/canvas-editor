1bb18e8ec27e05ff2692a2feba0eb9df9bc8dbee
Cluser <mrbfilipiak@gmail.com>
2020-08-08 00:21:18 +0200

    Fix editor components declaration

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EditorRoutingModule } from './editor-routing.module';
+import { EditorComponent } from './editor.component';
 import { EditorCanvasComponent } from './editor-canvas/editor-canvas.component';
 
 
 @NgModule({
-  declarations: [EditorCanvasComponent],
+  declarations: [
+    EditorComponent,
+    EditorCanvasComponent
+  ],
   imports: [
     CommonModule,
     EditorRoutingModule
