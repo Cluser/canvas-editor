6c429870c092a6f201dc30f6a21b14a01b7987c5
Cluser <mrbfilipiak@gmail.com>
2020-08-08 00:10:17 +0200

    Add fabric and some routing changes

--- src/app/editor/editor-routing.module.ts
+++ src/app/editor/editor-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditorComponent } from './editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EditorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
