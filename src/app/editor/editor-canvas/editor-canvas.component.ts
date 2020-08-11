adc9470d469ee8f165fb0e53340095be83dddde0
Cluser <mrbfilipiak@gmail.com>
2020-08-11 21:02:16 +0200

    Add canvas and some css

--- src/app/editor/editor-canvas/editor-canvas.component.ts
+++ src/app/editor/editor-canvas/editor-canvas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-editor-canvas',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorCanvasComponent implements OnInit {
 
+  private canvas: fabric.Canvas;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initCanvas();
+  }
+
+  private initCanvas(): void {
+    this.canvas = new fabric.Canvas('canvas', { preserveObjectStacking: true });
   }
 
 }
